6d1b117a9c2e308ee18fa73c12163b6c10ed4877
Islamiddin Islamov <islamov.isl168@gmail.com>
2023-02-02 20:16:30 +0600

    Rewrote the multiplication table

--- Cycles/task_5.py
+++ Cycles/task_5.py
@@ -1,7 +1,22 @@
+numbers = [i for i in range(1, 10)]
+lines = '-------------------------------'
 print('Таблица умножения')
-print('--------------------------')
+print(lines)
+print('| x |', end=' ')
+for i in numbers:
+    print(i, end='  ')
+print(f'\n{lines}')
 for n in range(1, 10):
+    print(f'| {n} |', end=' ')
     for m in range(1, 10):
-        print(n*m, end=' ')
+        s = n*m
+        if n < 5 and s < 8:
+            p = '  '
+        elif n == 1 and s == 8:
+            p = '  '  
+        else:
+            p = ' '    
+        print(s, end=p)
 
     print()    
+print(lines)
