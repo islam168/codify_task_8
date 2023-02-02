01561bc93ab5b5eaf91c93bc0f50bde173e7b399
Islamiddin Islamov <islamov.isl168@gmail.com>
2023-02-02 20:31:00 +0600

    added lines to the multiplication table

--- Cycles/task_5.py
+++ Cycles/task_5.py
@@ -1,10 +1,12 @@
-numbers = [i for i in range(1, 10)]
-lines = '-------------------------------'
+lines = '----+----------------------------'
+p = '  '
 print('Таблица умножения')
 print(lines)
 print('| x |', end=' ')
-for i in numbers:
-    print(i, end='  ')
+for i in range(1, 10):
+    if i == 9:
+        p = ' |'
+    print(i, end=p)
 print(f'\n{lines}')
 for n in range(1, 10):
     print(f'| {n} |', end=' ')
@@ -13,10 +15,10 @@ for n in range(1, 10):
         if n < 5 and s < 8:
             p = '  '
         elif n == 1 and s == 8:
-            p = '  '  
+            pass      
         else:
             p = ' '    
         print(s, end=p)
-
-    print()    
+    print('|')
+   
 print(lines)
